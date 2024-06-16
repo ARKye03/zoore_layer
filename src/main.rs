706a4d17735d7d7f2fed530dfa4a0a7526fd622f
ARKye03 <rafa2003sanchez@gmail.com>
2024-06-16 19:20:04 -0400

    [WIP] Add styling
    Need Hot Reload

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
-use gtk::{prelude::*, Label};
+use gtk::gdk::Display;
+use gtk::{prelude::*, CssProvider};
 use gtk4_layer_shell::{Edge, Layer, LayerShell};
+use std::fs;
+use std::process;
 
 // https://github.com/wmww/gtk-layer-shell/blob/master/examples/simple-example.c
 fn activate(application: &gtk::Application) {
@@ -35,7 +38,6 @@ fn activate(application: &gtk::Application) {
     }
 
     // Set up a widget
-    let label = gtk::Label::new(Some("Haaaaa"));
     let button = gtk::Button::new();
     let icon = gtk::Image::new();
     icon.set_from_file(Some("src/send.svg"));
@@ -44,13 +46,14 @@ fn activate(application: &gtk::Application) {
         println!("Hello")
     });
 
-    let master_center_box = gtk::CenterBox::new();
-    master_center_box.set_start_widget(Some(&button));
-    master_center_box.set_center_widget(Some(&label));
-    master_center_box.set_end_widget(Some(&Label::new(Some("XXDDDD"))));
+    let builder = gtk::Builder::from_file("src/ui/topbat.ui");
+    let master_center_box: gtk::CenterBox = builder
+        .object("GtkCenterBox")
+        .expect("Couldn't get GtkCenterBox");
 
     button.set_child(Some(&icon));
     window.set_child(Some(&master_center_box));
+    window.set_visible(true);
     window.present()
 }
 
@@ -60,6 +63,30 @@ fn main() {
     application.connect_activate(|app| {
         activate(app);
     });
+    application.connect_startup(|_| load_css());
 
     application.run();
+}
+
+fn load_css() {
+    // Load the CSS file and add it to the provider
+    let provider = CssProvider::new();
+    let input_scss = "src/styles/master.scss";
+    let output_css = "src/styles/style.css";
+
+    process::Command::new("sassc")
+        .arg(&input_scss)
+        .arg(&output_css)
+        .status()
+        .expect("Failed to run sassc");
+
+    let css = fs::read_to_string(output_css).expect("Failed to read CSS file");
+    provider.load_from_string(&css);
+
+    // Add the provider to the default screen
+    gtk::style_context_add_provider_for_display(
+        &Display::default().expect("Could not connect to a display."),
+        &provider,
+        gtk::STYLE_PROVIDER_PRIORITY_APPLICATION,
+    );
 }
