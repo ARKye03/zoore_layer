fe27179445ddc02c1bc615e6aa2a01202fc5ed49
ARKye03 <rafa2003sanchez@gmail.com>
2024-06-17 20:21:03 -0400

    re-structured workspace

--- src/main.rs
+++ src/main.rs
@@ -1,60 +1,15 @@
+mod top_bar;
+
 use gtk::gdk::Display;
 use gtk::{prelude::*, CssProvider};
-use gtk4_layer_shell::{Edge, Layer, LayerShell};
 use std::fs;
 use std::process;
 
 // https://github.com/wmww/gtk-layer-shell/blob/master/examples/simple-example.c
 fn activate(application: &gtk::Application) {
     // Create a normal GTK window however you like
-    let window = gtk::ApplicationWindow::new(application);
+    let window = top_bar::master::top_bar_window(application);
 
-    // Before the window is first realized, set it up to be a layer surface
-    window.init_layer_shell();
-
-    // Display above normal windows
-    window.set_layer(Layer::Top);
-
-    // Push other windows out of the way
-    window.auto_exclusive_zone_enable();
-
-    // The margins are the gaps around the window's edges
-    // Margins and anchors can be set like this...
-    window.set_margin(Edge::Left, 10);
-    window.set_margin(Edge::Right, 10);
-    window.set_margin(Edge::Bottom, 25);
-
-    // ... or like this
-    // Anchors are if the window is pinned to each edge of the output
-    let anchors = [
-        (Edge::Left, true),
-        (Edge::Right, true),
-        (Edge::Top, true),
-        (Edge::Bottom, false),
-    ];
-
-    for (anchor, state) in anchors {
-        window.set_anchor(anchor, state);
-    }
-
-    // Set up a widget
-    let button = gtk::Button::new();
-    let icon = gtk::Image::new();
-    icon.set_from_file(Some("src/send.svg"));
-    button.connect_clicked(|b| {
-        b.set_label("label");
-        println!("Hello")
-    });
-
-    let builder = gtk::Builder::from_file("src/ui/top_bar.ui");
-    let master_center_box: gtk::CenterBox = builder
-        .object("bar_CenterBox")
-        .expect("Couldn't get GtkCenterBox");
-
-    button.set_child(Some(&icon));
-    window.set_child(Some(&master_center_box));
-    window.set_namespace("top_bar");
-    window.set_widget_name("top_bar");
     window.present()
 }
 
