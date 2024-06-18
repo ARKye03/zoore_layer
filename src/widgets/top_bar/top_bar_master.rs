926f2e8d477ced9ae0509d2150c1443b6c67b667
ARKye03 <rafa2003sanchez@gmail.com>
2024-06-17 21:29:11 -0400

    feat: Update top bar UI with app launcher button and system button icons

--- src/widgets/top_bar/top_bar_master.rs
+++ src/widgets/top_bar/top_bar_master.rs
@@ -41,10 +41,20 @@ pub fn top_bar_window(application: &gtk::Application) -> ApplicationWindow {
         println!("Hello")
     });
 
-    let builder = gtk::Builder::from_file("src/top_bar/ui/top_bar.ui");
+    let builder = gtk::Builder::from_file("src/widgets/top_bar/ui/top_bar.ui");
     let master_center_box: gtk::CenterBox = builder
         .object("bar_CenterBox")
         .expect("Couldn't get GtkCenterBox");
+
+    let app_launcher_button: gtk::Button = builder
+        .object("app_launcher_button")
+        .expect("Couldn't get GtkButton app_launcher_button");
+
+    let app_launcher_button_icon: gtk::Image = builder
+        .object("app_launcher_button_icon")
+        .expect("Couldn't get GtkImage app_launcher_button_icon");
+    app_launcher_button_icon.set_pixel_size(25);
+    app_launcher_button_icon.set_from_file(Some("assets/applauncher.svg"));
 
     button.set_child(Some(&icon));
     window.set_child(Some(&master_center_box));
@@ -53,3 +63,53 @@ pub fn top_bar_window(application: &gtk::Application) -> ApplicationWindow {
 
     window
 }
+
+/* import Hyprland from "resource:///com/github/Aylur/ags/service/hyprland.js";
+import Widget from "resource:///com/github/Aylur/ags/widget.js";
+import { execAsync } from "resource:///com/github/Aylur/ags/utils.js";
+
+export const Workspaces = () =>
+  Widget.Box({
+    className: "workspaces",
+    setup: (self) => {
+      const Wicons = [
+        "",
+        " ",
+        " ",
+        "󰨞 ",
+        " ",
+        " ",
+        "󰭹 ",
+        " ",
+        " ",
+        "󰊖 ",
+        " ",
+      ];
+      const arr = Array.from({ length: 10 }, (_, i) => i + 1);
+
+      const updateWorkspaces = () => {
+        self.children = arr.map((i) => {
+          let className = ""; // default value
+
+          if (Hyprland.active.workspace.id === i) {
+            className = "focused";
+          } else if (
+            Hyprland.workspaces.some((ws) => ws.id === i && ws.windows > 0)
+          ) {
+            className = "work";
+          }
+
+          return Widget.Button({
+            onClicked: () =>
+              execAsync(`/usr/bin/hyprctl dispatch workspace ${i}`),
+            child: Widget.Label(`${Wicons[i]}`),
+            className: className,
+          });
+        });
+      };
+
+      self.hook(Hyprland.active.workspace, updateWorkspaces, "changed");
+      updateWorkspaces();
+    },
+  });
+ */
